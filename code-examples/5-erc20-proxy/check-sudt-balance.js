d0ddeafde851933f81044b6cc620887f8754b6b6
Flouse <flouse@gmail.com>
2022-05-27 14:20:03 +0000

    Update the GODWOKEN_WEB3_RPC of testnet_v1.1

--- code-examples/5-erc20-proxy/check-sudt-balance.js
+++ code-examples/5-erc20-proxy/check-sudt-balance.js
@@ -2,6 +2,7 @@ const { ethers } = require('hardhat');
 
 const ETHEREUM_ADDRESS = process.env.ETHEREUM_ADDRESS || '<YOUR_ETHEREUM_ADDRESS>';
 const SUDT_PROXY_CONTRACT_ADDRESS = process.env.SUDT_PROXY_CONTRACT_ADDRESS || '<YOUR_SUDT_PROXY_CONTRACT_ADDRESS>';
+const GODWOKEN_WEB3_RPC = process.env.GODWOKEN_WEB3_RPC || 'https://godwoken-testnet-v1.ckbapp.dev';
 
 (async () => {
     console.log(`Using Ethereum address: ${ETHEREUM_ADDRESS}`);
@@ -12,7 +13,7 @@ const SUDT_PROXY_CONTRACT_ADDRESS = process.env.SUDT_PROXY_CONTRACT_ADDRESS || '
         throw new Error('You need to fill <YOUR_ETHEREUM_ADDRESS> before starting the program.');
     }
 
-    const provider = new ethers.providers.JsonRpcProvider('https://godwoken-testnet-web3-v1-rpc.ckbapp.dev');
+    const provider = new ethers.providers.JsonRpcProvider(GODWOKEN_WEB3_RPC);
     const contract = (await ethers.getContractFactory('ERC20')).attach(SUDT_PROXY_CONTRACT_ADDRESS).connect(provider);
 
     console.log(await contract.callStatic.balanceOf(ETHEREUM_ADDRESS));
--- code-examples/5-erc20-proxy/index.js
+++ code-examples/5-erc20-proxy/index.js
@@ -3,6 +3,7 @@ const Web3 = require('web3');
 
 const CompiledContractArtifact = require(`./artifacts/contracts/SudtERC20Proxy.sol/ERC20.json`);
 
+const GODWOKEN_WEB3_RPC = process.env.GODWOKEN_WEB3_RPC || 'https://godwoken-testnet-v1.ckbapp.dev';
 const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY || '<YOUR_ETHEREUM_PRIVATE_KEY>'; // Replace this with your Ethereum private key with funds on Layer 2.
 const SUDT_ID = process.env.SUDT_ID || '<YOUR_SUDT_ID>'; // Replace this with SUDT ID received from depositing SUDT to Layer 2. This should be a number.
 const SUDT_NAME = process.env.SUDT_NAME || 'MyToken';
@@ -10,7 +11,7 @@ const SUDT_SYMBOL = process.env.SUDT_SYMBOL || 'MTK';
 const SUDT_TOTAL_SUPPLY = 9999999999;
 const SUDT_DECIMALS = 18; // Make sure this matches your token! Eg. for ckETH it is 18.
 
-const web3 = new Web3('https://godwoken-testnet-web3-v1-rpc.ckbapp.dev');
+const web3 = new Web3(GODWOKEN_WEB3_RPC);
 
 const account = web3.eth.accounts.wallet.add(ACCOUNT_PRIVATE_KEY);
 
